b4d15625b760958eca45a8ff0e021af67ef33821
2023Jado <tuyizjado1994@gmail.com>
2025-06-20 12:05:26 +0200

    ...

--- R/distwalk.R
+++ R/distwalk.R
@@ -59,10 +59,11 @@ distwalk <- function(file, Id_name, timestamp, crs_epsg) {
 
   # Automatically recognize and parse the format of timestamp
   formats_to_try <- c(
-    "%m/%d/%y %H:%M", "%m/%d/%y %H:%M:%S", "%m/%d/%y", "%d/%m/%y", "%y-%m-%d", "%m-%d-%y", "%d-%m-%y",
-    "%y-%m-%d %H:%M:%S", "%m-%d-%y %H:%M:%S", "%d-%m-%y %H:%M:%S",
+    "%m/%d/%y %H:%M", "%m/%d/%y %H:%M:%S", "%m/%d/%y", "%d/%m/%y", "%y-%m-%d",
+    "%m-%d-%y", "%d-%m-%y", "%d-%b-%y", "%d-%b-%Y", "%d %b %y", "%d %b %Y",
+    "%y-%m-%d %H:%M:%S", "%m-%d-%y %H:%M:%S", "%d-%m-%y %H:%M:%S", "%b-%d-%y", "%b-%d-%Y",
     "%m/%d/%y %H:%M:%S", "%d/%m/%y %H:%M:%S","%Y-%m-%d %H:%M:%S", "%Y-%m-%dT%H:%M:%S",
-    "%Y-%m-%dT%H:%M:%SZ", "%Y-%m-%d %H:%M:%OS", "%Y-%m-%dT%H:%M:%OS",
+    "%Y-%m-%dT%H:%M:%SZ", "%Y-%m-%d %H:%M:%OS", "%Y-%m-%dT%H:%M:%OS", "%b %d %y", "%b %d %Y",
     "%Y-%m-%dT%H:%M:%OSZ", "%Y-%m-%d", "%m/%d/%Y", "%d/%m/%Y", "%m-%d-%Y", "%d-%m-%Y",
     "%m/%d/%Y %H:%M:%S", "%d/%m/%Y %H:%M:%S", "%m-%d-%Y %H:%M:%S", "%d-%m-%Y %H:%M:%S",
     "%Y-%m-%d %I:%M:%S %p", "%m/%d/%Y %I:%M:%S %p", "%d/%m/%Y %I:%M:%S %p",
