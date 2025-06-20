c659c8ba67634c826a9ad706929989d4c7abd647
2023Jado <tuyizjado1994@gmail.com>
2025-06-20 12:33:26 +0200

    ...

--- R/homekde.R
+++ R/homekde.R
@@ -76,15 +76,14 @@ homekde <- function(file, crs_epsg, Id_name, timestamp, perc, parh){
 
   # Automatically recognize and parse the format of timestamp
   formats_to_try <- c(
-    "%m/%d/%y %H:%M", "%m/%d/%y %H:%M:%S", "%m/%d/%y", "%d/%m/%y", "%y-%m-%d",
+    "%m/%d/%y %H:%M", "%m/%d/%Y %H:%M", "%m/%d/%y %H:%M:%S", "%m/%d/%y", "%d/%m/%y", "%y-%m-%d",
     "%m-%d-%y", "%d-%m-%y", "%d-%b-%y", "%d-%b-%Y", "%d %b %y", "%d %b %Y",
     "%y-%m-%d %H:%M:%S", "%m-%d-%y %H:%M:%S", "%d-%m-%y %H:%M:%S", "%b-%d-%y", "%b-%d-%Y",
-    "%m/%d/%y %H:%M:%S", "%d/%m/%y %H:%M:%S","%Y-%m-%d %H:%M:%S", "%Y-%m-%dT%H:%M:%S",
-    "%Y-%m-%dT%H:%M:%SZ", "%Y-%m-%d %H:%M:%OS", "%Y-%m-%dT%H:%M:%OS", "%b %d %y", "%b %d %Y",
-    "%Y-%m-%dT%H:%M:%OSZ", "%Y-%m-%d", "%m/%d/%Y", "%d/%m/%Y", "%m-%d-%Y", "%d-%m-%Y",
-    "%m/%d/%Y %H:%M:%S", "%d/%m/%Y %H:%M:%S", "%m-%d-%Y %H:%M:%S", "%d-%m-%Y %H:%M:%S",
-    "%Y-%m-%d %I:%M:%S %p", "%m/%d/%Y %I:%M:%S %p", "%d/%m/%Y %I:%M:%S %p",
-    "%y-%m-%d %H:%M:%S", "%m/%d/%y %H:%M:%S", "%d/%m/%y %H:%M:%S",
+    "%d/%m/%y %H:%M:%S","%Y-%m-%d %H:%M:%S", "%Y-%m-%dT%H:%M:%S", "%Y-%m-%dT%H:%M:%SZ",
+    "%Y-%m-%d %H:%M:%OS", "%Y-%m-%dT%H:%M:%OS", "%b %d %y", "%b %d %Y", "%Y-%m-%dT%H:%M:%OSZ",
+    "%Y-%m-%d", "%m/%d/%Y", "%d/%m/%Y", "%m-%d-%Y", "%d-%m-%Y", "%m/%d/%Y %H:%M:%S",
+    "%d/%m/%Y %H:%M:%S", "%m-%d-%Y %H:%M:%S", "%d-%m-%Y %H:%M:%S", "%Y-%m-%d %I:%M:%S %p",
+    "%m/%d/%Y %I:%M:%S %p", "%d/%m/%Y %I:%M:%S %p", "%y-%m-%d %H:%M:%S", "%d/%m/%y %H:%M:%S",
     "excel_serial"
   )
 
